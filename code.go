03bcfc3da251637d8f30c584257ea7f1fec7231e
Chandler Newman <chandler2newman@hotmail.co.uk>
2023-11-06 17:02:57 +0000

    Add numerous op nodes

--- code.go
+++ code.go
@@ -3,7 +3,6 @@ package dextk
 import (
 	"errors"
 	"fmt"
-	"log"
 	"math"
 )
 
@@ -123,6 +122,35 @@ func (n CheckCastOpNode) String() string {
 	return fmt.Sprintf("%v ref=%v type=%v", n.Raw.Code(), n.Ref, n.Type)
 }
 
+type InstanceOfOpNode struct {
+	Raw  Op
+	Src  uint8
+	Dst  uint8
+	Type TypeDescriptor
+}
+
+func (n InstanceOfOpNode) RawOp() Op {
+	return n.Raw
+}
+
+func (n InstanceOfOpNode) String() string {
+	return fmt.Sprintf("%v src=%v dst=%v type=%v", n.Raw.Code(), n.Src, n.Dst, n.Type)
+}
+
+type ArrayLenOpNode struct {
+	Raw Op
+	Ref uint8
+	Dst uint8
+}
+
+func (n ArrayLenOpNode) RawOp() Op {
+	return n.Raw
+}
+
+func (n ArrayLenOpNode) String() string {
+	return fmt.Sprintf("%v ref=%v dst=%v type=%v", n.Raw.Code(), n.Ref, n.Dst)
+}
+
 type NewInstanceOpNode struct {
 	Raw  Op
 	Dst  uint8
@@ -137,6 +165,62 @@ func (n NewInstanceOpNode) String() string {
 	return fmt.Sprintf("%v dst=%v type=%v", n.Raw.Code(), n.Dst, n.Type)
 }
 
+type NewArrayOpNode struct {
+	Raw     Op
+	Dst     uint8
+	SizeReg uint8
+	Type    TypeDescriptor
+}
+
+func (n NewArrayOpNode) RawOp() Op {
+	return n.Raw
+}
+
+func (n NewArrayOpNode) String() string {
+	return fmt.Sprintf("%v dst=%v size=%v type=%v", n.Raw.Code(), n.Dst, n.SizeReg, n.Type)
+}
+
+type FilledNewArrayOpNode struct {
+	Raw       Op
+	Type      TypeDescriptor
+	ValueRegs []uint16
+}
+
+func (n FilledNewArrayOpNode) RawOp() Op {
+	return n.Raw
+}
+
+func (n FilledNewArrayOpNode) String() string {
+	return fmt.Sprintf("%v type=%v values=%v", n.Raw.Code(), n.Type, n.ValueRegs)
+}
+
+type FillArrayDataOpNode struct {
+	Raw Op
+	Ref uint8
+	FmtFillArrayDataPayload
+}
+
+func (n FillArrayDataOpNode) RawOp() Op {
+	return n.Raw
+}
+
+func (n FillArrayDataOpNode) String() string {
+	return fmt.Sprintf("%v ref=%v %v", n.Raw.Code(), n.Ref, n.FmtFillArrayDataPayload)
+}
+
+type ThrowOpNode struct {
+	Raw Op
+	Reg uint8
+}
+
+func (n ThrowOpNode) RawOp() Op {
+	return n.Raw
+}
+
+func (n ThrowOpNode) String() string {
+	return fmt.Sprintf("%v reg=%v", n.Raw.Code(), n.Reg)
+}
+
 type GotoOpNode struct {
 	Raw Op
 	Tgt int
@@ -148,6 +232,51 @@ func (n GotoOpNode) RawOp() Op {
 
 func (n GotoOpNode) String() string {
 	return fmt.Sprintf("%v tgt=%v", n.Raw.Code(), n.Tgt)
+}
+
+type PackedSwitchOpNode struct {
+	Raw      Op
+	Reg      uint8
+	FirstKey int32
+	Targets  []int
+}
+
+func (n PackedSwitchOpNode) RawOp() Op {
+	return n.Raw
+}
+
+func (n PackedSwitchOpNode) String() string {
+	return fmt.Sprintf("%v first=%v tgts=%v", n.Raw.Code(), n.FirstKey, n.Targets)
+}
+
+type SparseSwitchOpNode struct {
+	Raw     Op
+	Reg     uint8
+	Keys    []int32
+	Targets []int
+}
+
+func (n SparseSwitchOpNode) RawOp() Op {
+	return n.Raw
+}
+
+func (n SparseSwitchOpNode) String() string {
+	return fmt.Sprintf("%v keys=%v tgts=%v", n.Raw.Code(), n.Keys, n.Targets)
+}
+
+type CmpOpNode struct {
+	Raw  Op
+	SrcA uint8
+	SrcB uint8
+	Dst  uint8
+}
+
+func (n CmpOpNode) RawOp() Op {
+	return n.Raw
+}
+
+func (n CmpOpNode) String() string {
+	return fmt.Sprintf("%v a=%v b=%v tgt=%v", n.Raw.Code(), n.SrcA, n.SrcB, n.Dst)
 }
 
 type IfTestOpNode struct {
@@ -164,6 +293,36 @@ func (n IfTestOpNode) RawOp() Op {
 
 func (n IfTestOpNode) String() string {
 	return fmt.Sprintf("%v a=%v b=%v tgt=%v", n.Raw.Code(), n.A, n.B, n.Tgt)
+}
+
+type AGetOpNode struct {
+	Raw   Op
+	Dst   uint8
+	Array uint8
+	Index uint8
+}
+
+func (n AGetOpNode) RawOp() Op {
+	return n.Raw
+}
+
+func (n AGetOpNode) String() string {
+	return fmt.Sprintf("%v array=%v index=%v dst=%v", n.Raw.Code(), n.Array, n.Index, n.Dst)
+}
+
+type APutOpNode struct {
+	Raw   Op
+	Src   uint8
+	Array uint8
+	Index uint8
+}
+
+func (n APutOpNode) RawOp() Op {
+	return n.Raw
+}
+
+func (n APutOpNode) String() string {
+	return fmt.Sprintf("%v array=%v index=%v src=%v", n.Raw.Code(), n.Array, n.Index, n.Src)
 }
 
 type IGetOpNode struct {
@@ -227,7 +386,7 @@ func (n SPutOpNode) String() string {
 type InvokeOpNode struct {
 	Raw    Op
 	Method MethodRef
-	Args   []uint8
+	Args   []uint16
 }
 
 func (n InvokeOpNode) RawOp() Op {
@@ -236,6 +395,62 @@ func (n InvokeOpNode) RawOp() Op {
 
 func (n InvokeOpNode) String() string {
 	return fmt.Sprintf("%v method=%v args=%v", n.Raw.Code(), n.Method, n.Args)
+}
+
+type UnaryOpNode struct {
+	Raw Op
+	Src uint8
+	Dst uint8
+}
+
+func (n UnaryOpNode) RawOp() Op {
+	return n.Raw
+}
+
+func (n UnaryOpNode) String() string {
+	return fmt.Sprintf("%v src=%v dst=%v", n.Raw.Code(), n.Src, n.Dst)
+}
+
+type BinaryOpNode struct {
+	Raw  Op
+	SrcA uint8
+	SrcB uint8
+	Dst  uint8
+}
+
+func (n BinaryOpNode) RawOp() Op {
+	return n.Raw
+}
+
+func (n BinaryOpNode) String() string {
+	return fmt.Sprintf("%v srca=%v srcb=%v dst=%v", n.Raw.Code(), n.SrcA, n.SrcB, n.Dst)
+}
+
+type BinaryLiteralOpNode struct {
+	Raw    Op
+	SrcA   uint8
+	ValueB int16
+	Dst    uint8
+}
+
+func (n BinaryLiteralOpNode) RawOp() Op {
+	return n.Raw
+}
+
+func (n BinaryLiteralOpNode) String() string {
+	return fmt.Sprintf("%v srca=%v valueb=%v dst=%v", n.Raw.Code(), n.SrcA, n.ValueB, n.Dst)
+}
+
+type UnknownOpNode struct {
+	Raw Op
+}
+
+func (n UnknownOpNode) RawOp() Op {
+	return n.Raw
+}
+
+func (n UnknownOpNode) String() string {
+	return fmt.Sprintf("%v (unknown)", n.Raw.Code())
 }
 
 type CodeNode struct {
@@ -292,7 +507,7 @@ func (r *Reader) ReadCodeAndParse(off uint32) (CodeNode, error) {
 		var parsed OpNode
 
 		switch ot := o.(type) {
-		case *OpNop:
+		case *OpNop, *OpPseudoPackedSwitchPayload, *OpPseudoSparseSwitchPayload, *OpPseudoFillArrayDataPayload:
 			parsed = &NopOpNode{
 				Raw: ot,
 			}
@@ -504,6 +719,27 @@ func (r *Reader) ReadCodeAndParse(off uint32) (CodeNode, error) {
 				Ref:  data.A,
 				Type: val,
 			}
+		case *OpInstanceOf:
+			data := (ot.Fmt()).(Fmt22c)
+			val, err := r.ReadTypeAndParse(uint32(data.C))
+			if err != nil {
+				return res, err
+			}
+
+			parsed = &InstanceOfOpNode{
+				Raw:  ot,
+				Dst:  data.A,
+				Src:  data.B,
+				Type: val,
+			}
+		case *OpArrayLength:
+			data := (ot.Fmt()).(Fmt12x)
+
+			parsed = &ArrayLenOpNode{
+				Raw: ot,
+				Dst: data.A,
+				Ref: data.B,
+			}
 		case *OpNewInstance:
 			data := (ot.Fmt()).(Fmt21c)
 
@@ -520,6 +756,121 @@ func (r *Reader) ReadCodeAndParse(off uint32) (CodeNode, error) {
 				Raw:  ot,
 				Dst:  data.A,
 				Type: parsedDesc.ClassName,
+			}
+		case *OpNewArray:
+			data := (ot.Fmt()).(Fmt22c)
+
+			parsedDesc, err := r.ReadTypeAndParse(uint32(data.C))
+			if err != nil {
+				return res, fmt.Errorf("bad class type: %w", err)
+			}
+
+			if !parsedDesc.IsArray() {
+				return res, fmt.Errorf("%w: %v: invalid descriptor", ErrBadOp, ot)
+			}
+
+			parsed = &NewArrayOpNode{
+				Raw:     ot,
+				Dst:     data.A,
+				SizeReg: data.B,
+				Type:    parsedDesc,
+			}
+		case *OpFilledNewArray:
+			data := (ot.Fmt()).(Fmt35c)
+
+			parsedDesc, err := r.ReadTypeAndParse(uint32(data.B))
+			if err != nil {
+				return res, fmt.Errorf("bad class type: %w", err)
+			}
+
+			if !parsedDesc.IsArray() {
+				return res, fmt.Errorf("%w: %v: invalid descriptor", ErrBadOp, ot)
+			}
+
+			values := make([]uint16, data.A)
+
+			if data.A >= 1 {
+				values[0] = uint16(data.C)
+			}
+
+			if data.A >= 2 {
+				values[1] = uint16(data.D)
+			}
+
+			if data.A >= 3 {
+				values[2] = uint16(data.E)
+			}
+
+			if data.A >= 4 {
+				values[3] = uint16(data.F)
+			}
+
+			if data.A >= 5 {
+				values[4] = uint16(data.G)
+			}
+
+			parsed = &FilledNewArrayOpNode{
+				Raw:       ot,
+				Type:      parsedDesc,
+				ValueRegs: values,
+			}
+		case *OpFilledNewArrayRange:
+			data := (ot.Fmt()).(Fmt3rc)
+
+			parsedDesc, err := r.ReadTypeAndParse(uint32(data.B))
+			if err != nil {
+				return res, fmt.Errorf("bad class type: %w", err)
+			}
+
+			if !parsedDesc.IsArray() {
+				return res, fmt.Errorf("%w: %v: invalid descriptor", ErrBadOp, ot)
+			}
+
+			values := make([]uint16, data.A)
+
+			for i := uint8(0); i < data.A; i++ {
+				values[i] = data.C + uint16(i)
+			}
+
+			parsed = &FilledNewArrayOpNode{
+				Raw:       ot,
+				Type:      parsedDesc,
+				ValueRegs: values,
+			}
+		case *OpFillArrayData:
+			data := (ot.Fmt()).(Fmt31t)
+
+			tgt := pos + int(int32(data.B))
+			if tgt < 0 || tgt >= len(idMap) {
+				return res, fmt.Errorf("%w: invalid data offset %v", ErrBadOp, tgt)
+			}
+
+			oldPos := or.Pos()
+			or.Seek(tgt)
+
+			rtop, err := or.Read()
+			if err != nil {
+				return res, fmt.Errorf("%w: invalid data %w", ErrBadOp, err)
+			}
+
+			tableOp, ok := (rtop).(*OpPseudoFillArrayDataPayload)
+			if !ok {
+				return res, fmt.Errorf("%w: invalid data %v", ErrBadOp, tableOp)
+			}
+
+			or.Seek(oldPos)
+
+			parsed = &FillArrayDataOpNode{
+				Raw:                     ot,
+				Ref:                     data.A,
+				FmtFillArrayDataPayload: tableOp.FmtFillArrayDataPayload,
+			}
+		case *OpThrow:
+			data := (ot.Fmt()).(Fmt11x)
+
+			parsed = &ThrowOpNode{
+				Raw: ot,
+				Reg: data.A,
 			}
 		case *OpGoto:
 			data := (ot.Fmt()).(Fmt10t)
@@ -572,6 +923,85 @@ func (r *Reader) ReadCodeAndParse(off uint32) (CodeNode, error) {
 				Raw: ot,
 				Tgt: int(id),
 			}
+		case *OpPackedSwitch:
+			data := (ot.Fmt()).(Fmt31t)
+
+			tgt := pos + int(int32(data.B))
+			if tgt < 0 || tgt >= len(idMap) {
+				return res, fmt.Errorf("%w: invalid table offset %v", ErrBadOp, tgt)
+			}
+
+			oldPos := or.Pos()
+			or.Seek(tgt)
+
+			rtop, err := or.Read()
+			if err != nil {
+				return res, fmt.Errorf("%w: invalid table %w", ErrBadOp, err)
+			}
+
+			tableOp, ok := (rtop).(*OpPseudoPackedSwitchPayload)
+			if !ok {
+				return res, fmt.Errorf("%w: invalid table %v", ErrBadOp, tableOp)
+			}
+
+			or.Seek(oldPos)
+
+			tgts := make([]int, len(tableOp.Targets))
+
+			for i, target := range tableOp.Targets {
+				tgts[i] = pos + int(target)
+			}
+
+			parsed = &PackedSwitchOpNode{
+				Raw:      ot,
+				Reg:      data.A,
+				FirstKey: tableOp.FirstKey,
+				Targets:  tgts,
+			}
+		case *OpSparseSwitch:
+			data := (ot.Fmt()).(Fmt31t)
+
+			tgt := pos + int(int32(data.B))
+			if tgt < 0 || tgt >= len(idMap) {
+				return res, fmt.Errorf("%w: invalid table offset %v", ErrBadOp, tgt)
+			}
+
+			oldPos := or.Pos()
+			or.Seek(tgt)
+
+			rtop, err := or.Read()
+			if err != nil {
+				return res, fmt.Errorf("%w: invalid table %w", ErrBadOp, err)
+			}
+
+			tableOp, ok := (rtop).(*OpPseudoSparseSwitchPayload)
+			if !ok {
+				return res, fmt.Errorf("%w: invalid table %v", ErrBadOp, tableOp)
+			}
+
+			or.Seek(oldPos)
+
+			tgts := make([]int, len(tableOp.Targets))
+
+			for i, target := range tableOp.Targets {
+				tgts[i] = pos + int(target)
+			}
+
+			parsed = &SparseSwitchOpNode{
+				Raw:     ot,
+				Reg:     data.A,
+				Keys:    tableOp.Keys,
+				Targets: tgts,
+			}
+		case *OpCmplFloat, *OpCmpgFloat, *OpCmplDouble, *OpCmpgDouble, *OpCmpLong:
+			data := (ot.Fmt()).(Fmt23x)
+
+			parsed = &CmpOpNode{
+				Raw:  ot,
+				SrcA: data.B,
+				SrcB: data.C,
+				Dst:  data.A,
+			}
 		case *OpIfEq, *OpIfNe, *OpIfLt, *OpIfGe, *OpIfGt, *OpIfLe:
 			data := (ot.Fmt()).(Fmt22t)
 
@@ -609,6 +1039,24 @@ func (r *Reader) ReadCodeAndParse(off uint32) (CodeNode, error) {
 				A:   data.A,
 				B:   -1,
 				Tgt: int(id),
+			}
+		case *OpAget, *OpAgetWide, *OpAgetObject, *OpAgetBoolean, *OpAgetByte, *OpAgetChar, *OpAgetShort:
+			data := (ot.Fmt()).(Fmt23x)
+
+			parsed = &AGetOpNode{
+				Raw:   ot,
+				Dst:   data.A,
+				Array: data.B,
+				Index: data.C,
+			}
+		case *OpAput, *OpAputWide, *OpAputObject, *OpAputBoolean, *OpAputByte, *OpAputChar, *OpAputShort:
+			data := (ot.Fmt()).(Fmt23x)
+
+			parsed = &APutOpNode{
+				Raw:   ot,
+				Src:   data.A,
+				Array: data.B,
+				Index: data.C,
 			}
 		case *OpIget, *OpIgetWide, *OpIgetObject, *OpIgetBoolean, *OpIgetByte, *OpIgetChar, *OpIgetShort:
 			data := (ot.Fmt()).(Fmt22c)
@@ -672,26 +1120,26 @@ func (r *Reader) ReadCodeAndParse(off uint32) (CodeNode, error) {
 				return res, err
 			}
 
-			args := make([]uint8, data.A)
+			args := make([]uint16, data.A)
 
 			if data.A >= 1 {
-				args[0] = data.C
+				args[0] = uint16(data.C)
 			}
 
 			if data.A >= 2 {
-				args[1] = data.D
+				args[1] = uint16(data.D)
 			}
 
 			if data.A >= 3 {
-				args[2] = data.E
+				args[2] = uint16(data.E)
 			}
 
 			if data.A >= 4 {
-				args[3] = data.F
+				args[3] = uint16(data.F)
 			}
 
 			if data.A >= 5 {
-				args[4] = data.G
+				args[4] = uint16(data.G)
 			}
 
 			parsed = &InvokeOpNode{
@@ -699,9 +1147,89 @@ func (r *Reader) ReadCodeAndParse(off uint32) (CodeNode, error) {
 				Method: m,
 				Args:   args,
 			}
+		case *OpInvokeVirtualRange, *OpInvokeSuperRange, *OpInvokeDirectRange, *OpInvokeStaticRange,
+			*OpInvokeInterfaceRange:
+			data := (ot.Fmt()).(Fmt3rc)
 
+			m, err := r.ReadMethodAndParse(uint32(data.B))
+			if err != nil {
+				return res, err
+			}
+
+			args := make([]uint16, data.A)
+
+			for i := uint8(0); i < data.A; i++ {
+				args[i] = data.C + uint16(i)
+			}
+
+			parsed = &InvokeOpNode{
+				Raw:    ot,
+				Method: m,
+				Args:   args,
+			}
+		case *OpNegInt, *OpNotInt, *OpNegLong, *OpNotLong, *OpNegFloat, *OpNegDouble, *OpIntToLong, *OpIntToFloat,
+			*OpIntToDouble, *OpLongToInt, *OpLongToFloat, *OpLongToDouble, *OpFloatToInt, *OpFloatToLong,
+			*OpFloatToDouble, *OpDoubleToInt, *OpDoubleToLong, *OpDoubleToFloat, *OpIntToByte, *OpIntToChar,
+			*OpIntToShort:
+			data := (ot.Fmt()).(Fmt12x)
+
+			parsed = &UnaryOpNode{
+				Raw: ot,
+				Dst: data.A,
+				Src: data.B,
+			}
+		case *OpAddInt, *OpSubInt, *OpMulInt, *OpDivInt, *OpRemInt, *OpAndInt, *OpOrInt, *OpXorInt, *OpShlInt,
+			*OpShrInt, *OpUshrInt, *OpAddLong, *OpSubLong, *OpMulLong, *OpDivLong, *OpRemLong, *OpAndLong, *OpOrLong,
+			*OpXorLong, *OpShlLong, *OpShrLong, *OpUshrLong, *OpAddFloat, *OpSubFloat, *OpMulFloat, *OpDivFloat,
+			*OpRemFloat, *OpAddDouble, *OpSubDouble, *OpMulDouble, *OpDivDouble, *OpRemDouble:
+			data := (ot.Fmt()).(Fmt23x)
+
+			parsed = &BinaryOpNode{
+				Raw:  ot,
+				Dst:  data.A,
+				SrcA: data.B,
+				SrcB: data.C,
+			}
+		case *OpAddInt2Addr, *OpSubInt2Addr, *OpMulInt2Addr, *OpDivInt2Addr, *OpRemInt2Addr, *OpAndInt2Addr,
+			*OpOrInt2Addr, *OpXorInt2Addr, *OpShlInt2Addr, *OpShrInt2Addr, *OpUshrInt2Addr, *OpAddLong2Addr,
+			*OpSubLong2Addr, *OpMulLong2Addr, *OpDivLong2Addr, *OpRemLong2Addr, *OpAndLong2Addr, *OpOrLong2Addr,
+			*OpXorLong2Addr, *OpShlLong2Addr, *OpShrLong2Addr, *OpUshrLong2Addr, *OpAddFloat2Addr, *OpSubFloat2Addr,
+			*OpMulFloat2Addr, *OpDivFloat2Addr, *OpRemFloat2Addr, *OpAddDouble2Addr, *OpSubDouble2Addr,
+			*OpMulDouble2Addr, *OpDivDouble2Addr, *OpRemDouble2Addr:
+			data := (ot.Fmt()).(Fmt12x)
+
+			parsed = &BinaryOpNode{
+				Raw:  ot,
+				Dst:  data.A,
+				SrcA: data.A,
+				SrcB: data.B,
+			}
+		case *OpAddIntLit16, *OpRsubIntLit16, *OpMulIntLit16, *OpDivIntLit16, *OpRemIntLit16, *OpAndIntLit16,
+			*OpOrIntLit16, *OpXorIntLit16:
+			data := (ot.Fmt()).(Fmt22s)
+
+			parsed = &BinaryLiteralOpNode{
+				Raw:    ot,
+				Dst:    data.A,
+				SrcA:   data.B,
+				ValueB: int16(data.C),
+			}
+		case *OpAddIntLit8, *OpRsubIntLit8, *OpMulIntLit8, *OpDivIntLit8, *OpRemIntLit8, *OpAndIntLit8, *OpOrIntLit8,
+			*OpXorIntLit8, *OpShlIntLit8, *OpShrIntLit8, *OpUshrIntLit8:
+			data := (ot.Fmt()).(Fmt22b)
+
+			parsed = &BinaryLiteralOpNode{
+				Raw:    ot,
+				Dst:    data.A,
+				SrcA:   data.B,
+				ValueB: int16(int8(data.C)),
+			}
+		// TODO: invoke-polymorphic, invoke-polymorphic/range, invoke-custom, invoke-custom/range, const-method-handle,
+		//       const-method-type
 		default:
-			log.Panic(o)
+			parsed = &UnknownOpNode{
+				Raw: ot,
+			}
 		}
 
 		res.Ops[idPos] = parsed
